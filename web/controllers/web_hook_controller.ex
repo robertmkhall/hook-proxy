1173e51c2707d62372835e6c24e357dd7efd2d8e
rha29 <robert.hall@sky.uk>
2016-03-04 20:54:16 +0000

    output connection details

--- web/controllers/web_hook_controller.ex
+++ web/controllers/web_hook_controller.ex
@@ -2,6 +2,7 @@ defmodule HookProxy.WebHookController do
   use HookProxy.Web, :controller
 
   def forward(conn, _params) do
+    IO.inspect conn
     print_body Plug.Conn.read_body(conn)
     Plug.Conn.send_resp(conn, 200, "Success!")
   end
