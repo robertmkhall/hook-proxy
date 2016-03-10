0afbae37e74ab5f668556dd435ce0590bd194dc9
rha29 <robert.hall@sky.uk>
2016-03-10 09:01:15 +0000

    @robertmkhall add github emoji icon

--- web/adapters/github_to_slack.ex
+++ web/adapters/github_to_slack.ex
@@ -3,32 +3,18 @@ defmodule HookProxy.GithubToSlackAdapter do
   alias HookProxy.GitHubWebhook, as: Webhook
 
   def slack_request(conn) do
-    slack_request(request_type(conn), request_body(conn))
+    slack_request(request_type(conn.req_headers), conn.body_params)
   end
 
-  defp request_type(conn) do
-    conn
-    |> request_headers
-    |> request_type_header_val
-  end
-
-  defp request_type_header_val([]) do
+  defp request_type([]) do
     :not_recognised
   end
 
-  defp request_type_header_val([head | tail]) do
+  defp request_type([head | tail]) do
     case head do
       {"x-github-event", type} -> type
-      _ -> request_type_header_val(tail)
+      _ -> request_type(tail)
     end
-  end
-
-  defp request_headers(conn) do
-    Map.get(conn, :req_headers)
-  end
-
-  defp request_body(conn) do
-    Map.get(conn, :body_params)
   end
 
   defp slack_request(type, payload) do
@@ -39,7 +25,7 @@ defmodule HookProxy.GithubToSlackAdapter do
   defp request_json(type, payload) do
     %{
       "username": "github",
-      "icon_emoji": ":trollface:",
+      "icon_emoji": ":github:",
       "text": custom_message,
       "mrkdwn": true,
       "attachments": [
@@ -70,7 +56,7 @@ defmodule HookProxy.GithubToSlackAdapter do
   end
 
   defp webhook_type_message(type, payload) do
-    "[#{Webhook.repo_name(payload)}] #{format_request_type(type)} submitted by #{Webhook.user_login(payload)}"
+    "[#{Webhook.Repo.name(payload)}] #{format_request_type(type)} submitted by <#{Webhook.User.url(payload)}|#{Webhook.User.login(payload)}>"
   end
 
   defp format_request_type(type) do
--- web/webhooks/git_hub_webhook.ex
+++ web/webhooks/git_hub_webhook.ex
@@ -1,25 +1,33 @@
 defmodule HookProxy.GitHubWebhook do
-  def repo(payload, key) do
-    payload
-    |> Map.get("repository")
-    |> Map.get(key)
-  end
-
-  def repo_name(payload) do
-    repo(payload, "full_name")
-  end
-
-  def user(payload, key) do
-    payload
-    |> Map.get("sender")
-    |> Map.get(key)
-  end
-
-  def user_login(payload) do
-    user(payload, "login")
-  end
-
   def number(payload) do
     Map.get(payload, "number")
+  end
+
+  defmodule Repo do
+    def repo(payload, key) do
+      payload
+      |> Map.get("repository")
+      |> Map.get(key)
+    end
+
+    def name(payload) do
+      repo(payload, "full_name")
+    end
+  end
+
+  defmodule User do
+    def user(payload, key) do
+      payload
+      |> Map.get("sender")
+      |> Map.get(key)
+    end
+
+    def login(payload) do
+      user(payload, "login")
+    end
+
+    def url(payload) do
+      user(payload, "html_url")
+    end
   end
 end
